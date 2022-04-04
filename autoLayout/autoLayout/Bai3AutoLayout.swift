f8741194e309d79b6fce8f3f5ccdd6d35a815999
Dong Duong V <dong.duong@supremetech.vn>
2022-04-04 17:11:28 +0700

    AUTOLAYOUT

---
+++ autoLayout/autoLayout/Bai3AutoLayout.swift
@@ -0,0 +1,29 @@
+//
+//  Bai3AutoLayout.swift
+//  autoLayout
+//
+//  Created by Dong Duong V. on 02/04/2022.
+//
+
+import UIKit
+
+class Bai3AutoLayout: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
